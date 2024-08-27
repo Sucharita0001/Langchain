61d10eef00f142abee750bf06def318d63bc47c5
Sucharita0001 <sucharita.935@gmail.com>
2024-08-27 16:06:53 +0530

    [VERSION 2] Adding custom prompt

--- open_chat_model.py
+++ open_chat_model.py
@@ -1,12 +1,23 @@
 from langchain_community.chat_models import ChatOllama
-import streamlit as st
+from langchain.prompts import PromptTemplate
 
 llm = ChatOllama(model="gemma:2b")
 
-st.title('Ask your query')
+difficulty = input("Enter your difficulty level: ")
+topic = input("Enter topic: ")
+promptTemplate = PromptTemplate(
+    template="""
+    You are a master of Java. 
+    There are three different difficulty levels, Beginner, Intermediate and Expert, to check any individual's knowledge. 
+    Generate 5 MCQs for {difficulty_level} difficulty on topic {topic}.
+    """
+)
 
-question = st.text_input("Enter the question: ")
-
-if question:
-    response = llm.invoke(question)
-    st.write(response.content)
+if difficulty:
+    if topic:
+        response = llm.invoke(promptTemplate.format(difficulty_level=difficulty, topic = topic))
+        print(response.content)
+    else:
+        print("Enter valid topic.")
+else:
+    print("Please give a valid difficulty level.")
